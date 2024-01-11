ef0b89330ad42d114b2aa6d81381b9bbd7b73db0
AnkitGrin <ankit@softradix.com>
2024-01-11 15:40:19 +0530

    select field done

--- components/mainContent.jsx
+++ components/mainContent.jsx
@@ -2,27 +2,40 @@ import React, { useEffect, useState } from "react";
 import styles from "../styles/MainContent.module.css";
 import { Button, Container } from "react-bootstrap";
 import Select from "./select";
+import MultiSelect from "./multiSelect";
 
 const MainContent = () => {
-  const [selectValue, setSelectValue] = useState("");
-  useEffect(() => {
-    console.log("selectValue ==> ", selectValue);
-  }, [selectValue]);
+  const [selectTableValue, setSelectTableValue] = useState("");
+
   const optionsList = [
     { title: "Cup", value: 1 },
     { title: "Bottle", value: 2 },
     { title: "Chair", value: 3 },
   ];
+  const fieldsOptionList = ["C++", "Java", "ReactJS", "Spring Boot"];
+
+  const [fieldsItem, setFieldsItem] = useState([]);
+
   return (
     <Container>
       <div className={styles.mainWrap}>
         <Select
           label="Table name"
           optionsList={optionsList}
-          value={selectValue}
-          onChange={setSelectValue}
+          value={selectTableValue}
+          onChange={setSelectTableValue}
           placeholder="Select"
         />
+        <div className={styles.selectFieldWrap}>
+          Select{" "}
+          <MultiSelect
+            selectedItems={fieldsItem}
+            setSelectedItems={setFieldsItem}
+            optionList={fieldsOptionList}
+            placeHolder="Fields"
+          />{" "}
+          from <span className={styles.tableName}>{selectTableValue}</span>
+        </div>
       </div>
     </Container>
   );
